084ad7153cd40c9e78bd07c89a5c760e1ab9bfa7
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-03-03 10:53:39 -0500

    Create carbon-black-install.ps1

---
+++ apps/adobe-reader/carbon-black-install.ps1
@@ -0,0 +1,18 @@
+# Set the MSI path. Please only use one of these.
+$msiUrl = '@msiUrl@'
+$msiPath = '@msiPath@'
+$companyCode = '@companyCode@'
+$argumentList = '/qn ' +  $companyCode + ' HIDE_COMMAND_LINES=1'
+$ltSvcPath = %ltsvcdir%
+
+Start-Transcript -Path $ltSvcPath\carbon-black-install.log
+
+# Check if URL or MSI Path is set then run installer.
+if ($msiUrl) {
+
+    wget $msiUrl -OutFile '$ltSvcPath\packages\carbon-black.msi'
+    msi /i '$ltSvcPath\packages\carbon-black.msi' /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
+
+} else {
+    msiexec /i '$msiPath' /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
+}
