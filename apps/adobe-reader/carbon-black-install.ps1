da81abc83fbfe4f7515e9ad5656e4be435204c65
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-03-03 11:04:03 -0500

    Update carbon-black-install.ps1

--- apps/adobe-reader/carbon-black-install.ps1
+++ apps/adobe-reader/carbon-black-install.ps1
@@ -8,13 +8,13 @@ $ltSvcPath = %ltsvcdir%
 Start-Transcript -Path $ltSvcPath\carbon-black-install.log
 
 # Check if URL or MSI Path is set then run installer.
-if ($msiUrl) {
+if ($msiUrl -ne '@msiUrl@') {
 
-    wget $msiUrl -OutFile '$ltSvcPath\packages\carbon-black.msi'
-    msi /i '$ltSvcPath\packages\carbon-black.msi' /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
+    Invoke-WebRequest -Uri $msiUrl -OutFile $ltSvcPath'\packages\carbon-black.msi'
+    msiexec /i $ltSvcPath'\packages\carbon-black.msi' /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
 
 } else {
-    msiexec /i '$msiPath' /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
+    msiexec /i $msiPath /qn COMPANY_CODE=$companyCode HIDE_COMMAND_LINES=1
 }
 
 Stop-Transcript
