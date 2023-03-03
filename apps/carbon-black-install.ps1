187de7691502ae952d9393496053583eab30151e
Nate Smith <40123869+nettts@users.noreply.github.com>
2023-03-03 14:29:26 -0500

    Update carbon-black-install.ps1

--- apps/carbon-black-install.ps1
+++ apps/carbon-black-install.ps1
@@ -1,21 +1,24 @@
-Start-Transcript -Path $ltSvcDir'\'$packageFileName'.log'
+Start-Transcript -Path $ltSvcDir\$packageFileName.log
+
+
 $packageFileName = $packageName -Replace "\s", "-"
 # Check if Carbon Black is installed.
 $installed = Get-Package | Where {$_.Name -like "*$packageName*"}
 if ($installed) {
-    Write-Ouput "$packageName already installed. Exiting"
+    Write-Output "$packageName already installed. Exiting"
     exit  
 }
 
 # Check if URL or MSI Path is set then run installer.
 if ($msiUrl -ne '@msiUrl@') {
     Write-Output "msiUrl variable has data. We're going to download the msi and execute."
-    Invoke-WebRequest -Uri $msiUrl -OutFile $ltSvcDir'\packages\$packageFileName.msi'
-    msiexec /i $ltSvcDir'\packages\$packageFileName.msi ' + $msiArguments 
+    Invoke-WebRequest -Uri $msiUrl -OutFile '$ltSvcDir\packages\$packageFileName.msi'
+    msiexec /i '$ltSvcDir\packages\$packageFileName.msi ' + $msiArguments 
 
 } else {
     Write-Output "msiUrl is blank. Launching msi from msiPath."
-    msiexec /i $msiPath + ' ' + $msiArugments
+    $msiFlag = '/i ' + $msiPath + ' ' + $msiArguments
+    Start-Process msiexec.exe -ArgumentList $msiFlag
 }
 
 Stop-Transcript
