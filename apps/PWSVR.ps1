63cdb121ce5747fb651d50e981f157c399dbd4f2
scottlei4dtctoday <131402416+scottlei4dtctoday@users.noreply.github.com>
2023-04-27 14:46:34 -0400

    Local testing

--- apps/PWSVR.ps1
+++ apps/PWSVR.ps1
@@ -2,4 +2,4 @@ Get-Process pwsvr -ErrorAction SilentlyContinue | Stop-Process -Force
 $pwsvrdir = get-itempropertyvalue -path 'HKLM:\Software\wow6432Node\PWInc\PWSvr\' -name PWSvrDir
 Remove-Item $pwsvrdir\* -recurse
 Copy-Item "C:\pwsvrbackup\*" $pwsvrdir
-Start-Process $pwsvrdir\"pwsvr.exe"
+#Start-Process $pwsvrdir\"pwsvr.exe"
