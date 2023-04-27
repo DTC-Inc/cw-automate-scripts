4eb4dea58c9814d32af7bf7bda3729807f9c25f2
scottlei4dtctoday <131402416+scottlei4dtctoday@users.noreply.github.com>
2023-04-27 15:01:21 -0400

    Update PWSVR.ps1

--- apps/PWSVR.ps1
+++ apps/PWSVR.ps1
@@ -1,5 +1,13 @@
+# Stop PWSvr.exe if running
 Get-Process pwsvr -ErrorAction SilentlyContinue | Stop-Process -Force
+# Get PWSvr directory from the registry
 $pwsvrdir = get-itempropertyvalue -path 'HKLM:\Software\wow6432Node\PWInc\PWSvr\' -name PWSvrDir
+# Delete all files in the production PWSvr directory
 Remove-Item $pwsvrdir\* -recurse
+# Copy files from backup folder to production folder
 Copy-Item "C:\pwsvrbackup\*" $pwsvrdir
-#Start-Process $pwsvrdir\"pwsvr.exe"
+# Set full path to pwsvr.exe as a variable
+$pwsvrexe = "PWSvr.exe"
+$PWSvrPathexe = $pwsvrdir $pwsvrexe
+# Start the pwsvr.exe
+Start-Process $PWSvrPathexe
