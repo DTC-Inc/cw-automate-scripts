a1843f932b4b5e8cbd721382ed14d0e53295e320
scottlei4dtctoday <131402416+scottlei4dtctoday@users.noreply.github.com>
2023-09-28 09:55:39 -0400

    Update SetPagefileSize.ps1
    Comments added

--- policy/SetPagefileSize.ps1
+++ policy/SetPagefileSize.ps1
@@ -1,7 +1,14 @@
+#Enable privelege to modify pagefile
 $computersys = Get-WmiObject Win32_ComputerSystem -EnableAllPrivileges
+
+#Set System Managed Pagefile to False
 $computersys.AutomaticManagedPagefile = $False
 $computersys.Put()
+
+#Query the pagefile settings
 $pagefileset = Get-WmiObject -Query "Select * From Win32_PageFileSetting Where Name like '%pagefile.sys'"
+
+#Set InitalSize and MaximumSize for PageFile
 $pagefileset.InitialSize = 1024
 $pagefileset.MaximumSize = 3072
 $pagefileset.Put()
